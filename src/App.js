aa7c493558fb059be2457af854669b5a78e0cd0e
M-Dinesh9258 <dineshmadalam9258@gmail.com>
2024-07-15 11:42:02 +0530

    master

--- src/App.js
+++ src/App.js
@@ -2,27 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 
-import  CustomButton from"./components/button/button"
-import MainHeading1 ,{ Heading2 as SecondaryHeading ,Heading3 }  from "./components/headings/headings"
-import Orderlist from './components/lists/orderlist';
-import Unorderlist from './components/lists/unorder';
-import Profile from './components/profile/profile';
-import Listfruits from './components/lists/fruits';
-
 function App() {
   return (
     <div className="">
-      <CustomButton/>
-      <h3>Hello !</h3>
-      <MainHeading1/>
-      <SecondaryHeading/>
-      <Heading3/>
-      <Orderlist/>
-      <Unorderlist/>
-      <Profile/>
-      <Listfruits/>
-     
-    
+      <h3>Hello !</h3>    
     </div>
   )
 }
