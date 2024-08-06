c78f13f143fe2551df9423e4512a7a97afdff658
Dinesh-madalam <dineshmadalam2k1@gmail.com>
2024-08-06 18:07:19 +0530

    added source files

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import IplData from './ipl_List/ipldata';
-import IplHeading from './ipl_List/iplheading';
-import IplImage from './ipl_List/iplimages';
-import IplCups from './ipl_List/iplcups';
-import Iplplayers from './ipl_List/iplplayers';
-import "./ipl.css"
-import CustomProgressBar from './components/bootstrap/progress-bar';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Customcard from './components/card/card';
+// import 'bootstrap/dist/bootstarp/min.css'
 
 
 
-const App =()=>(
-  <>  
-  <div>
-    <div className="nav">
-      <a>Home</a>
-      <a>Team Name</a>
-      <a>Team Logo</a>
-      <a>Cups Won</a>
-      <a>Team players</a>
+function App () {
+  return(
+    <div className='App'>
+    <Customcard/>
      
     </div>
-    <div>
-    <Customcard/>
-    </div>
-  </div>
-    
-    </>
-   
-
-
-    );
+  )
+}
 
 export default App;
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,124 +1,92 @@
-// import Heading1, { Heading2 } from "../headings/headings";
-// import CustomImage from "../image/image";
-import IplData from "../../ipl_List/ipldata"
-import IplHeading from "../../ipl_List/iplheading"
-import CustomProgressBar from "../bootstrap/progress-bar"
-import "./card.css"
-import { Component } from "react"
+import { Component } from 'react'
+import './card.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomProgressBar from '../bootstrap/progress-bar';
 
+class Customcard extends Component {
+    state = {
+        productlist : [],
+        loader : true,
+        error : false
+    }
 
-
-
-// const Card= (prop) =>{
-//   const {name,cups,image}=prop
-//   return(
-//     <div>
-//     <Heading1 data={name}></Heading1>
-//     <CustomImage data={image}></CustomImage>
-//     <Heading2 data={cups}></Heading2>
-//     </div>
-//   )
-  
-// };
-
-// export default Card;
-
-
-class Customcard extends Component{
-
-  state = {
-      array : IplData,
-  }
-
-  incrementhandler=(targetteam)=> {
-      const Updatedlist = this.state.array.map((each) => {
-          if(each.name==targetteam){
-              return {...each, cups : each.cups+1}
-          }else{
-              return {...each}
-          }
-      }) 
-
-      const newState = Updatedlist;
-
-      this.setState({
-          array : newState,
-      })
-  }
-
-  decrementhandler=(targetteam, wins)=> {
-      const Updatedlist = this.state.array.map((each) => {
-          if(each.name==targetteam){
-              return {...each, cups : each.cups-1}
-          }else{
-              return {...each}
-          }
-      })
-
-      this.setState({
-          array : Updatedlist,
-      })
-  }
-
-  percent(cups){
-      var p = (cups/17)*100;
-      return p;
-  }
-
-  render() {
-
-
-  return(
-     <>
-           <>
-            {
-                this.state.array.map((each) => {
-                    return(
-                        <div className='body'>
-                          <div key={each.team} style={{height:"500px", width:"300px", border:"2px solid black",margin:10}}>
-                        <IplHeading team={each.team}/>
-                        <img src={each.image} style={{width:"200px", height:"200px"}}/>
-                        <h3>{each.name}</h3>
-                        {/* <button>See more...</button><br/> */}
-                        <CustomProgressBar scale={this.percent(each.cups)}/>
-                        <br/>
-                        <button onClick={() => this.incrementhandler(each.name)}>Increment</button>
-                        <button onClick={() => this.decrementhandler(each.name, each.cups)}>Decrement</button><br/><br/>
-                        </div>
-                        </div>
-                    );
+    fetchHandler(){
+        fetch("https://fakestoreapi.com/products")
+        .then((response) => response.json())
+        .then(((data) =>{ 
+            // console.log(data)
+            return (
+                this.setState({
+                    productlist : data,
+                    loader : false
                 })
+            );
+        }))
+        
+    }
+
+    Precentage = (rate,base) =>{
+        var percent = (rate/base)*100
+        return percent;
+    }
+
+    IncrementHandler = (targetProductId) => {
+        const UpdatedProduct = this.state.productlist.map((each) => {
+            if(each.id === targetProductId){
+                return {...each, rating : {...each.rating, rate : each.rating.rate + 1}}
+            }
+            else{
+                return {...each}
+            }
+        })
+
+        this.setState({
+            productlist : UpdatedProduct
+        })
+    }
+    
+    DecrementHalndler = (targetProductId) => {
+        const UpdatedProduct = this.state.productlist.map((each) => {
+            if(each.id === targetProductId){
+                return {...each, rating: {...each.rating, rate : each.rating.rate - 1}}
+            }
+            else{
+                return {...each}
+            }
+        })
+
+        this.setState({
+            productlist : UpdatedProduct
+        })
+    }
+
+    render() {
+
+        return(
+            <>
+            {
+                this.state.loader ? 
+                <>
+                <h1>Please wait</h1>
+                <button onClick={() => this.fetchHandler()}>click</button>
+                </>: 
+                <div className='flex'>
+                {
+                    this.state.productlist.map((eachProduct) => <div className='card'>
+                        <h6>{eachProduct.title}</h6>
+                        <img src={eachProduct.image} className='image'/>
+                        <p>{`Rating : ${eachProduct.rating.rate}`}</p><br/>
+                        <CustomProgressBar scale={this.Precentage(eachProduct.rating.rate,5)}/>
+                        <button onClick={()=> this.IncrementHandler(eachProduct.id)}>Like</button>
+                        <button onClick={()=> this.DecrementHalndler(eachProduct.id)}>Dislike</button>
+                        </div>)
+                }
+                </div>
             }
             </>
-     </>
-  )
+        )
+    }
 }
-}
-  //          {
-  //             this.state.array.map((eachipl) => (
-                 
-  //                   <div className="body">
-                   
-  //                     <div key={eachipl.team} style={{height:"500px", width:"300px", border:"2px solid black",margin:10}} >
-  //                       <IplHeading team={eachipl.team}/>
-  //                       <IplImage
-  //                       source={eachipl.image}
-  //                       alternate="ipl image"
-  //                       width={200}
-  //                       height={200}/>
-  //                       <IplCups cups={eachipl.cups}/>
-  //                       <Iplplayers players={eachipl.players}/>
-  //                       {/* <CustomProgressBar scale={eachipl.cups} style={{width:"100px"}}/> */}
-  //                       <Customcard/>
-                      
-                      
-  //                     </div>
-  //                  )}
-    
-  //                    </div> 
-  //              }
-  //          )
-  //    }          
 
 
 export default Customcard;
