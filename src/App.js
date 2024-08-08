6f2f972510106940e2e86e0116c99bc6efd05879
Dinesh Madalam <107678459+Dinesh-madalam@users.noreply.github.com>
2024-08-08 23:41:37 +0530

    conditional-rendering

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import IplData from './ipl_List/ipldata';
-import IplHeading from './ipl_List/iplheading';
-import IplImage from './ipl_List/iplimages';
-import IplCups from './ipl_List/iplcups';
-import Iplplayers from './ipl_List/iplplayers';
-import "./ipl.css"
-import CustomProgressBar from './components/bootstrap/progress-bar';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Customcard from './components/card/card';
+import Conditional from '.conditional'
 
 
-
-const App =()=>(
-  <>  
+function App(){
+  const arr=[1,2,4,4]
+return (
   <div>
-    <div className="nav">
-      <a>Home</a>
-      <a>Team Name</a>
-      <a>Team Logo</a>
-      <a>Cups Won</a>
-      <a>Team players</a>
-     
-    </div>
-    <div>
-    <Customcard/>
-    </div>
+    <Conditional array={arr}/>
   </div>
-    
-    </>
-   
+)
 
 
-    );
+}
 
 export default App;
